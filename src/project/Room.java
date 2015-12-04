7ea565afc9d80faf4f14e1f6785ebe18c323c4d5
Ken Schnall <kenneth.schnall@gmail.com>
2015-12-03 23:58:15 -0500

    First commit

---
+++ src/project/Room.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author KAS6570
+ */
+public class Room {
+    int number;
+    ArrayList<Meeting> meetings = new ArrayList<Meeting>();
+    // cannot hold more than one meeting in one time slot
+    Room(int number) {
+        this.number = number;
+    }
+}
